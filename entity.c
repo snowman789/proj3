434050e5d348d7c593e678c1cf5a551a7ef09c21
isaac <itr9f@virginia.edu>
2019-10-08 14:06:34 -0400

    Starting files

---
+++ entity.c
@@ -0,0 +1,75 @@
+/******************************************************************************/
+/*                                                                            */
+/* ENTITY IMPLEMENTATIONS                                                     */
+/*                                                                            */
+/******************************************************************************/
+
+// Student names:
+// Student computing IDs:
+//
+//
+// This file contains the actual code for the functions that will implement the
+// reliable transport protocols enabling entity "A" to reliably send information
+// to entity "B".
+//
+// This is where you should write your code, and you should submit a modified
+// version of this file.
+//
+// Notes:
+// - One way network delay averages five time units (longer if there are other
+//   messages in the channel for GBN), but can be larger.
+// - Packets can be corrupted (either the header or the data portion) or lost,
+//   according to user-defined probabilities entered as command line arguments.
+// - Packets will be delivered in the order in which they were sent (although
+//   some can be lost).
+// - You may have global state in this file, BUT THAT GLOBAL STATE MUST NOT BE
+//   SHARED BETWEEN THE TWO ENTITIES' FUNCTIONS. "A" and "B" are simulating two
+//   entities connected by a network, and as such they cannot access each
+//   other's variables and global state. Entity "A" can access its own state,
+//   and entity "B" can access its own state, but anything shared between the
+//   two must be passed in a `pkt` across the simulated network. Violating this
+//   requirement will result in a very low score for this project (or a 0).
+//
+// To run this project you should be able to compile it with something like:
+//
+//     $ gcc entity.c simulator.c -o myproject
+//
+// and then run it like:
+//
+//     $ ./myproject 0.0 0.0 10 500 3 test1.txt
+//
+// Of course, that will cause the channel to be perfect, so you should test
+// with a less ideal channel, and you should vary the random seed. However, for
+// testing it can be helpful to keep the seed constant.
+//
+// The simulator will write the received data on entity "B" to a file called
+// `output.dat`.
+
+#include <stdio.h>
+#include "simulator.h"
+
+/**** A ENTITY ****/
+
+void A_init() {
+}
+
+void A_output(struct msg message) {
+}
+
+void A_input(struct pkt packet) {
+}
+
+void A_timerinterrupt() {
+}
+
+
+/**** B ENTITY ****/
+
+void B_init() {
+}
+
+void B_input(struct pkt packet) {
+}
+
+void B_timerinterrupt() {
+}
